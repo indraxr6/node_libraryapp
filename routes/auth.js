b7e836a20466a6fc5ceda6943fae9792b8a0aff7
Indra W <indra.roadota@gmail.com>
2022-02-25 07:47:49 +0700

    third

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,10 @@
 const express = require('express')
-const { sign } = require('jsonwebtoken') // <--
+
+const controller = require('./../controllers/auth')
+
 const router = express.Router()
 
-router.post('/login', function (req, res) {
-    let credentials = {
-        username: req.body.username,
-        password: req.body.password
-    }
+router.post('/login', controller.login)
+router.post('/register', controller.register)
 
-    if (!credentials.username || !credentials.password) {
-        return res.send('Username dan/atau Password harus diisi!')
-    }
-
-    if (credentials.username != 'admin' || credentials.password != 'admin') {
-        return res.send('Username dan/atau Password tidak sesuai')
-    }
-
-    token = sign(credentials, 'verysecretkey') // <--
-    
-    return res.send({ // <--
-        token: token,
-        data: credentials
-    })
-})
-
-module.exports = router 
+module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,12 @@
 const express = require('express')
 
 const verifyToken = require('./../verify-token')
-const controller = require('./../controllers/home') // <--
+const controller = require('./../controllers/home')
 
 const router = express.Router()
 
-router.get('/', controller.index) // <--
+router.get('/', controller.index)
 
-router.get('/home', verifyToken, function (req, res) {
-    res.send({
-        user: req.user
-    })
-})
+router.get('/home', verifyToken, controller.home) // <--
 
 module.exports = router
